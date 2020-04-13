110d12ce3af492f326fb1a880e0574c0e73f7b17
Jordan Michele <jordanmichele32@gmail.com>
2020-04-13 14:26:52 -0400

    Updates

---
+++ precache-manifest.fe87e9f3ba381b88f4f1d96b95acf2f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9e75e67356b67b550a6c8d67a7895a",
+    "url": "/chartingApp/index.html"
+  },
+  {
+    "revision": "92967e3f31f6228d09c5",
+    "url": "/chartingApp/static/css/main.7ddcb16e.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/chartingApp/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "92967e3f31f6228d09c5",
+    "url": "/chartingApp/static/js/main.02e0797a.chunk.js"
+  },
+  {
+    "revision": "f9a1db39dd750f3e3402",
+    "url": "/chartingApp/static/js/runtime~main.bf93beb1.js"
+  }
+]);
